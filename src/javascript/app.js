69968519b04a43897cd97252f55cff94644c3efc
William Barela <william.barela.dev@gmail.com>
2020-05-06 00:08:22 -0500

    move code of app into IIFE leaving imports outside

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,9 +1,11 @@
 import { Bass } from './instruments/bass.js';
 import { Scale } from './theory/scale.js';
 
-const myBass = new Bass(6);
-console.log(myBass.strings);
+(() => {
+  const myBass = new Bass(6);
+  console.log(myBass.strings);
 
-const myScale = new Scale("C", "Aeolian");
-console.log(myScale.root);
-console.log(myScale.mode);
+  const myScale = new Scale("C", "Aeolian");
+  console.log(myScale.root);
+  console.log(myScale.mode);
+})()
