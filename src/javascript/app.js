08acb7232b91b75c76fe7c423eced7587edeed59
William Barela <william.barela.dev@gmail.com>
2020-05-05 23:50:29 -0500

    add import for Scale class and test that it works

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,6 +1,9 @@
-import { Bass, scales } from './instruments/bass.js';
-
-console.log(scales);
+import { Bass } from './instruments/bass.js';
+import { Scale } from './theory/scale.js';
 
 const myBass = new Bass(6);
 console.log(myBass.strings);
+
+const myScale = new Scale("C", "Aeolian");
+console.log(myScale.root);
+console.log(myScale.mode);
