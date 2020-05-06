85a657d8a89f58a3880458cd9611b85c2a69abaf
William Barela <william.barela.dev@gmail.com>
2020-05-06 15:49:35 -0500

    testing

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -5,7 +5,9 @@ import { Scale } from './theory/scale.js';
   const myBass = new Bass(6);
   console.log(myBass.strings);
 
-  const myScale = new Scale("C", "Aeolian");
+  const myScale = new Scale("E", "Phrygian");
   console.log(myScale.root);
   console.log(myScale.mode);
+  console.log(myScale.rootChromaticInterval);
+  console.log(myScale.modeNotes)
 })()
