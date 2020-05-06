295f51ccd977e056197235499958fcf8f0fda876
William Barela <william.barela.dev@gmail.com>
2020-05-06 18:25:43 -0500

    add test for scale

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,13 +2,9 @@ import { Bass } from './instruments/bass.js';
 import { Scale } from './theory/scale.js';
 
 (() => {
-  const myBass = new Bass(6);
-  console.log(myBass.strings);
-
-  const myScale = new Scale("B", "Phrygian");
+  // Test Scales
+  let myScale = new Scale("E", "Phrygian");
   console.log(myScale.root);
-  console.log(myScale.mode);
-  console.log(myScale.rootChromaticInterval);
-  console.log(myScale.parentScaleNotes)
-  console.log(myScale.modeNotes)
+  console.log(myScale.parentScaleNotes);
+  console.log(myScale.modeNotes);
 })()
