020aea0bdc5cf5e12f548765d43f133fafef4b84
William Barela <william.barela.dev@gmail.com>
2020-05-08 18:35:36 -0500

    DOMcontentLoaded Display.banner using scales-banner id

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,14 @@ import { Scale } from './Scale.js';
 import { Adapter } from "./Adapter.js";
 import { Display } from "./Display.js";
 
-(() => {
-  // Test Scales
-  const another_scale = new Scale("A", "VI");
-  console.log(another_scale.modeInfo);
-  console.log(another_scale.parentKeyNotes);
-  console.log(another_scale.notes);
-  console.log(another_scale.intervals);
-})()
+window.addEventListener('DOMContentLoaded', (e) => {
+  Display.banner();
+});
+
+// Test Scales
+const another_scale = new Scale("A", "VI");
+console.log(another_scale.modeInfo);
+console.log(another_scale.parentKeyNotes);
+console.log(another_scale.notes);
+console.log(another_scale.intervals);
 
