0344bce9a56fc1898c9ea2ea6452c2007a223b4e
William Barela <william.barela.dev@gmail.com>
2020-05-10 16:58:39 -0500

    add event listeners to root and mode selectors

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,32 @@ const scale = new Scale("A", "III");
 
 Display.load();
 Display.scaleCard(scale);
-setTimeout( () => {
-  const scale1 = new Scale("B", "III");
-  Display.scaleCard(scale1);
-}, 5000)
+
+// const changeRoot = (rootNoteValue) => {
+//   console.log(rootNoteValue);
+//   const scale = new Scale(rootNoteValue);
+//   Display.scaleCard(scale);
+// };
+// 
+// const rootNote = document.getElementById("scale-root-note");
+// rootNote.addEventListener("change", changeRoot(rootNote.value), false); 
+
+
+const rootNoteSelected = document.getElementById("scale-root-note");
+const modeSelected = document.getElementById("scale-root-mode");
+
+rootNoteSelected.addEventListener('change', (e) => {
+  const scale = new Scale(e.target.value, modeSelected.value);
+  Display.scaleCard(scale);
+});
+
+modeSelected.addEventListener('change', (e) => {
+  const scale = new Scale(rootNoteSelected.value, e.target.value);
+  Display.scaleCard(scale);
+});
+
+
+// setTimeout( () => {
+//   const scale1 = new Scale("B", "III");
+//   Display.scaleCard(scale1);
+// }, 5000)
