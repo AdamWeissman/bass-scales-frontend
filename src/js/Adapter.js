8f8bbe2cc6f5c6b421502b47224a92398a4f13b1
William Barela <william.barela.dev@gmail.com>
2020-05-18 00:03:52 -0500

    add post adapter method

--- src/js/Adapter.js
+++ src/js/Adapter.js
@@ -7,4 +7,12 @@ export class Adapter {
     return fetch(this.baseUrl + endpoint)
            .then(response => response.json())
   }
+
+  post(endpoint, data) {
+    return fetch(this.baseUrl + endpoint, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    }).then(response => response.json());
+  }
 }
